897ce7127fbd00da3c1953de499cc0872d38f685
Francois Zhao <francois.zhao@epitech.eu>
2022-10-28 17:56:39 +0700

    FIX style

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -66,7 +66,19 @@ function App() {
         aria-labelledby="modal-modal-title"
         aria-describedby="modal-modal-description"
       >
-        <Box sx={style}>
+        <Box
+          sx={{
+            position: "absolute",
+            top: "50%",
+            left: "50%",
+            transform: "translate(-50%, -50%)",
+            width: 400,
+            bgcolor: "background.paper",
+            border: "2px solid #000",
+            boxShadow: 24,
+            p: 4,
+          }}
+        >
           <Typography id="modal-modal-title" variant="h6" component="h2">
             Add a new weather widget
           </Typography>
@@ -93,15 +105,3 @@ function App() {
 }
 
 export default App;
-
-const style = {
-  position: "absolute",
-  top: "50%",
-  left: "50%",
-  transform: "translate(-50%, -50%)",
-  width: 400,
-  bgcolor: "background.paper",
-  border: "2px solid #000",
-  boxShadow: 24,
-  p: 4,
-};
